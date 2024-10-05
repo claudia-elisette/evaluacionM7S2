e407579d2159cd8115dac1a0d89968ede0f7fe87
Claudia Elisette <claudiae.urbinag@gmail.com>
2024-10-05 00:39:16 -0300

    home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    Home
+    <h1>Categorías</h1>
+    <div class="btnGroup">
+      <router-link to="/indie" tag="button">Indie</router-link>
+      <router-link to="/pop" tag="button">Pop</router-link>
+      <router-link to="/rock" tag="button">Rock</router-link>
+    </div>
   </div>
 </template>
 
@@ -13,3 +18,35 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .home{
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 1.875rem;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1.25rem;
+  }
+  h1{
+    font-size: 1.5rem;
+    margin: 0 auto;
+  }
+  .btnGroup{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    row-gap: 20px;
+    column-gap: 15px;
+  }
+  button{
+    width: 30%;
+    max-width: 300px;
+    min-width: 200px;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: green;
+    color: white;
+  }
+</style>
